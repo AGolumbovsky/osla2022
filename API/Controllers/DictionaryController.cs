d5d84caf14f21d06f37a5b80deb7e5a2b0ed2b8b
agolumbovsky <agolumbovsky@gmail.com>
2022-12-04 22:56:19 -0500

    added a Dict controller but it broke it

---
+++ API/Controllers/DictionaryController.cs
@@ -0,0 +1,33 @@
+// using System;
+// using System.Collections.Generic;
+// using System.Diagnostics;
+// using System.Linq;
+// using System.Threading.Tasks;
+// using Microsoft.AspNetCore.Mvc;
+// using Microsoft.Extensions.Logging;
+
+// namespace API.Controllers
+// {
+//     [ApiController]
+//     [Route("[controller]")]
+//     public class DictionaryController : ControllerBase
+//     {
+//         private readonly ILogger<DictionaryController> _logger;
+
+//         public DictionaryController(ILogger<DictionaryController> logger)
+//         {
+//             _logger = logger;
+//         }
+
+//         public IActionResult Index()
+//         {
+//             return View();
+//         }
+
+//         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+//         public IActionResult Error()
+//         {
+//             return View("Error!");
+//         }
+//     }
+// }
